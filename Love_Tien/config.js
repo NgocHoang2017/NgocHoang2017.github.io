b68fcecf8d9396d6f6b89b9e8fb45f26bb3720c8
unknown <hoangnn@dienquang.com>
2023-01-05 09:05:26 +0700

    for Lover

---
+++ Love_Tien/config.js
@@ -0,0 +1,17 @@
+
+var config = {
+    
+    lines: [
+        "CHÀO BÉ TIÊN TIÊN ĐÁNG YÊU",
+        "Em là người con gái anh thương nhất",
+        "Em xinh đẹp nhất !",
+        "Em đáng yêu nhất ! ",
+        "Anh cũng yêu em nhất",
+        "Em là một điều may mắn trong cuộc sống của anh ",
+        "Thương em  nhiều lắm...",
+    ],
+    
+    //date: "14-11-2022",
+    // 双方的名字
+    //names: [" TIÊN TIÊN <3"]
+}
